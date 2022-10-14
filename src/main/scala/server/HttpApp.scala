62b806f5bcf3c9f891789d050bd2a41ef25dbd16
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-14 20:42:26 +0900

    Add Transact[F[_], T]

--- src/main/scala/server/HttpApp.scala
+++ src/main/scala/server/HttpApp.scala
@@ -9,11 +9,15 @@ import org.http4s.dsl.io.*
 
 import lepus.router.{ *, given }
 import lepus.server.LepusApp
-import lepus.database.DatabaseConfig
+import lepus.database.{ DatabaseConfig, DBTransactor }
 
 import app.repository.{ TaskRepository, CategoryRepository, TaskCategoryRepository }
 import app.service.TaskService
 import app.controller.api.TaskController
+
+type Transact[F[_], T] = DBTransactor[F] ?=> T
+
+val id = bindPath[Long]("id")
 
 object HttpApp extends LepusApp[IO]:
 
@@ -21,20 +25,20 @@ object HttpApp extends LepusApp[IO]:
     DatabaseConfig("lepus.app.template://master/edu_todo")
   )
 
-  override def routes =
-    val taskRepository         = new TaskRepository
-    val categoryRepository     = new CategoryRepository
-    val taskCategoryRepository = new TaskCategoryRepository
+  val taskRepository:         Transact[IO, TaskRepository]         = new TaskRepository
+  val categoryRepository:     Transact[IO, CategoryRepository]     = new CategoryRepository
+  val taskCategoryRepository: Transact[IO, TaskCategoryRepository] = new TaskCategoryRepository
 
-    val taskService = new TaskService(taskRepository, taskCategoryRepository, categoryRepository)
-    val taskController = new TaskController(taskService)
-    NonEmptyList.of(
+  val taskService: Transact[IO, TaskService] = new TaskService(taskRepository, taskCategoryRepository, categoryRepository)
 
+  val taskController: Transact[IO, TaskController] = new TaskController(taskService)
+
+  override def routes = NonEmptyList.of(
     "tasks" ->> RouterConstructor.of {
       case GET  => taskController.get
       case POST => taskController.post(summon[Request[IO]])
     },
-    "tasks" / bindPath[Long]("id") ->> RouterConstructor.of {
+    "tasks" / id ->> RouterConstructor.of {
       case GET    => taskController.getById(summon[Long])
       case PUT    => taskController.put(summon[Long], summon[Request[IO]])
       case DELETE => taskController.delete(summon[Long])
@@ -43,7 +47,7 @@ object HttpApp extends LepusApp[IO]:
       case GET  => ???
       case POST => ???
     },
-    "categories" / bindPath[Long]("id") ->> RouterConstructor.of {
+    "categories" / id ->> RouterConstructor.of {
       case PUT    => ???
       case DELETE => ???
     }
