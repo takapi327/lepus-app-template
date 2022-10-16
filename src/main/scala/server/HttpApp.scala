832e418102e942be0ed288c9f9e9647c57841266
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-16 18:35:52 +0900

    Fix HttpApp

--- src/main/scala/server/HttpApp.scala
+++ src/main/scala/server/HttpApp.scala
@@ -11,7 +11,6 @@ import lepus.router.{ *, given }
 import lepus.server.LepusApp
 import lepus.database.{ DatabaseConfig, DBTransactor }
 
-import app.repository.{ TaskRepository, CategoryRepository, TaskCategoryRepository }
 import app.service.{ TaskService, CategoryService }
 import app.controller.api.{ TaskController, CategoryController }
 
@@ -21,36 +20,35 @@ val id = bindPath[Long]("id")
 
 object HttpApp extends LepusApp[IO]:
 
-  override val databases = Set(APP.eduTodo)
+  val db: DatabaseConfig = DatabaseConfig("lepus.database://edu_todo")
+
+  val eduTodo: Transact[IO, infrastructure.eduTodo.EduTodo] =
+    infrastructure.eduTodo.EduTodo(db)
+
+  override val databases = Set(db)
+
+  val taskService: Transact[IO, TaskService] = TaskService(eduTodo.taskRepository, eduTodo.categoryRepository, eduTodo.taskCategoryRepository)
+  val categoryService: Transact[IO, CategoryService] = CategoryService(eduTodo.categoryRepository)
+
+  val taskController: Transact[IO, TaskController] = TaskController(taskService)
+  val categoryController: Transact[IO, CategoryController] = CategoryController(categoryService)
 
   override def routes = NonEmptyList.of(
     "tasks" ->> RouterConstructor.of {
-      case GET  => APP.taskController.get
-      case POST => APP.taskController.post(summon[Request[IO]])
+      case GET  => taskController.get
+      case POST => taskController.post
     },
     "tasks" / id ->> RouterConstructor.of {
-      case GET    => APP.taskController.getById(summon[Long])
-      case PUT    => APP.taskController.put(summon[Long], summon[Request[IO]])
-      case DELETE => APP.taskController.delete(summon[Long])
+      case GET    => taskController.getById(summon[Long])
+      case PUT    => taskController.put(summon[Long])
+      case DELETE => taskController.delete(summon[Long])
     },
     "categories" ->> RouterConstructor.of {
-      case GET  => APP.categoryController.get
-      case POST => APP.categoryController.post(summon[Request[IO]])
+      case GET  => categoryController.get
+      case POST => categoryController.post
     },
     "categories" / id ->> RouterConstructor.of {
-      case PUT    => APP.categoryController.put(summon[Long], summon[Request[IO]])
-      case DELETE => APP.categoryController.delete(summon[Long])
+      case PUT    => categoryController.put(summon[Long])
+      case DELETE => categoryController.delete(summon[Long])
     }
   )
-
-object APP:
-
-  val eduTodo: DatabaseConfig = DatabaseConfig("lepus.database://edu_todo")
-
-  val categoryRepository: Transact[IO, CategoryRepository] = CategoryRepository(eduTodo)
-
-  val taskService: Transact[IO, TaskService] = new TaskService(TaskRepository(eduTodo), categoryRepository, TaskCategoryRepository(eduTodo))
-  val categoryService: Transact[IO, CategoryService] = new CategoryService(categoryRepository)
-
-  val taskController: Transact[IO, TaskController] = new TaskController(taskService)
-  val categoryController: Transact[IO, CategoryController] = new CategoryController(categoryService)
