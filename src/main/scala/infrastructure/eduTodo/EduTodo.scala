f3cd3958b13120272645fe75d3b335e803b43a77
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-22 14:13:58 +0900

    Add defaultDB

--- src/main/scala/infrastructure/eduTodo/EduTodo.scala
+++ src/main/scala/infrastructure/eduTodo/EduTodo.scala
@@ -10,12 +10,12 @@ import lepus.database.{ DatabaseConfig, DBTransactor, Transact, DatabaseModule }
 
 import infrastructure.eduTodo.repository.{ CategoryRepository, TaskCategoryRepository, TaskRepository }
 
-case class EduTodo(database: DatabaseConfig)(using DBTransactor[IO]) extends DatabaseModule[IO]
+case class EduTodo(database: DatabaseConfig, defaultDB: String)(using DBTransactor[IO]) extends DatabaseModule[IO]
 
 object EduTodo:
   val db: DatabaseConfig = DatabaseConfig("lepus.database://edu_todo", NonEmptyList.of("master", "slave"))
 
-  given Transact[IO, EduTodo] = EduTodo(db)
+  given Transact[IO, EduTodo] = EduTodo(db, "slave")
 
   val taskRepository:         Transact[IO, TaskRepository]         = TaskRepository()
   val categoryRepository:     Transact[IO, CategoryRepository]     = CategoryRepository()
