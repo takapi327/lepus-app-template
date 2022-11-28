85062d11fc7f0c18d62a5c0e41b6b20146975d05
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-11-28 20:25:43 +0900

    Fix HttpApp

--- src/main/scala/server/HttpApp.scala
+++ src/main/scala/server/HttpApp.scala
@@ -11,8 +11,6 @@ import lepus.router.{ *, given }
 import lepus.server.LepusApp
 import lepus.logger.{ LoggerF, LoggingIO, given }
 
-import infrastructure.databases.eduTodo
-
 import presentation.controller.*
 
 val id = bindPath[Long]("id")
@@ -21,25 +19,23 @@ object HttpApp extends LepusApp[IO], LoggingIO:
 
   given LoggerF[IO] = logger
 
-  override val databases = Set(eduTodo.db)
-
   override val routes = NonEmptyList.of(
     "tasks" ->> RouterConstructor.of {
-      case GET  => taskController.get
-      case POST => taskController.post
+      case GET  => TaskController().get
+      case POST => TaskController().post
     },
     "tasks" / id ->> RouterConstructor.of {
-      case GET    => taskController.getById
-      case PUT    => taskController.put
-      case DELETE => taskController.delete
+      case GET    => TaskController().getById
+      case PUT    => TaskController().put
+      case DELETE => TaskController().delete
     },
     "categories" ->> RouterConstructor.of {
-      case GET  => categoryController.get
-      case POST => categoryController.post
+      case GET  => CategoryController().get
+      case POST => CategoryController().post
     },
     "categories" / id ->> RouterConstructor.of {
-      case PUT    => categoryController.put
-      case DELETE => categoryController.delete
+      case PUT    => CategoryController().put
+      case DELETE => CategoryController().delete
     }
   )
 
